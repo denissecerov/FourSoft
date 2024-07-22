be549821b9ddf10392a8724bdbe73fff97048ad7
Alvin Cheon <108594108+alvrnicu@users.noreply.github.com>
2024-07-22 00:01:56 -0400

    testing push

--- taskly/settings.py
+++ taskly/settings.py
@@ -10,6 +10,15 @@ SECRET_KEY = 'your-secret-key'
 DEBUG = True
 ALLOWED_HOSTS = []
 
+# Email Backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'foursoftfoursoft@gmail.com'
+EMAIL_HOST_PASSWORD = 'imohfsfnimfllgzf' # App Password
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
